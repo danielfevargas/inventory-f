4a3035f0206019b6a252dd907eb7077d92a7e9cf
danielfe2005 <daniel.vargas1@estudiantesunibague.edu.co>
2026-04-09 15:57:39 -0500

    hecho y listo"

---
+++ src/infrastructure/config/container.js
@@ -0,0 +1,13 @@
+// import InMemoryProductRepository from "../repositories/InMemoryProductRepository.js";
+import CreateProduct         from "../../application/usecases/CreateProduct.js";
+import GetAllProducts        from "../../application/usecases/GetAllProducts.js";
+import GetProductBySku       from "../../application/usecases/GetProductBySku.js";
+import DeleteProduct         from "../../application/usecases/DeleteProduct.js";
+import ApiProductRepository from "../repositories/ApiProductRepository.js";
+
+const repository = new ApiProductRepository();
+
+export const createProduct   = new CreateProduct(repository);
+export const getAllProducts   = new GetAllProducts(repository);
+export const getProductBySku = new GetProductBySku(repository);
+export const deleteProduct   = new DeleteProduct(repository);
